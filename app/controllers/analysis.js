cad01136b745f85e93b8bb477b8411e3c0feae3c
lgybetter <437675103@qq.com>
2017-03-26 20:05:29 +0800

    add some annotation

--- app/controllers/analysis.js
+++ app/controllers/analysis.js
@@ -54,30 +54,39 @@ function judgeType(word) {
   }
 }
 
+
+//判断是否是字母与下划线
 function judgeWord(word) {
   let wordPatten = /[a-z]|[A-z]|\_/
   return wordPatten.test(word)
 }
 
+//判断是否为数字
 function judgeNumber(number) {
   let numberPatten = /[0-9]/
   return numberPatten.test(number)
 }
 
+//判断是否为特殊字符
 function judgeSpecialWord(letter) {
   return wordsMap.get(letter) === 'specialWord' ? true : false
 }
 
+//判断是否为关键词
 function judgeKeyWord(letter) {
   return wordsMap.get(letter) === 'keyWord' ? true : false
 }
 
+/**
+ * 分析每行的数据
+ */
 exports.analysisLine = (line, annotation) => {
   let oneLine = []
   let word = ''
   let i = 0
   let includeFile = false
   while (i < line.length) {
+    //注释代码的优先级最高，需要先进行判断
     if (line[i] === '/' && line[i + 1] === '*') {
       word += line[i]
       i++
@@ -86,28 +95,33 @@ exports.analysisLine = (line, annotation) => {
     if (!annotation) {
       //表示不是注释内容
       if (judgeWord(line[i])) {
+        //表示是属于字母与下划线
         word += line[i]
         while (i + 1 < line.length && (judgeNumber(line[i + 1]) || judgeWord(line[i + 1]))) {
           i++
           word += line[i]
         }
         if (judgeKeyWord(word)) {
+          //判断单词是否属于关键词
           oneLine.push({
             word: word,
             type: 'keyWord'
           })
         } else {
+          //不为关键词, 则为标识符
           oneLine.push({
             word: word,
             type: 'identifier'
           })
         }
+        //由于include是属于头文件的引入, 需要对头文件进行特殊处理
         if (word === 'include') {
           includeFile = true
         }
         word = ''
         i++
       } else if (line[i] === '"') {
+        //字符串判断
         while (i + 1 < line.length && line[i + 1] !== '"') {
           word += line[i]
           i++
@@ -120,6 +134,7 @@ exports.analysisLine = (line, annotation) => {
         word = ''
         i += 2
       } else if (line[i] === '\'') {
+        //字符判断
         while (i + 1 < line.length && line[i + 1] !== '\'') {
           word += line[i]
           i++
@@ -132,6 +147,7 @@ exports.analysisLine = (line, annotation) => {
         word = ''
         i += 2
       } else if (judgeNumber(line[i])) {
+        //数字判断
         word += line[i]
         while (i + 1 < line.length && (judgeNumber(line[i + 1]) || line[i + 1] === '.')) {
           i++
@@ -144,7 +160,9 @@ exports.analysisLine = (line, annotation) => {
         word = ''
         i++
       } else if (judgeSpecialWord(line[i])) {
+        //特殊字符判断
         if (line[i] === '<' && includeFile) {
+          //处理头文件的引入
           oneLine.push({
             word: line[i],
             type: 'specialWord'
@@ -162,6 +180,7 @@ exports.analysisLine = (line, annotation) => {
           word = ''
           i++
         } else {
+          //处理//的注释代码
           if (line[i] === '/' && line[i + 1] === '/') {
             i++
             while (i + 1 < line.length) {
